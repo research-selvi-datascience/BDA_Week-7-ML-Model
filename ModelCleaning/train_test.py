2e08666ea981d981b99bf8fe749a821d8794f8ec
research-selvi-datascience <selvi.digitalsolutions@gmail.com>
2026-05-16 09:35:35 +0000

    Added mlflow

--- ModelCleaning/train_test.py
+++ ModelCleaning/train_test.py
@@ -1,9 +1,12 @@
 import os
+import mlflow
+import mlflow.sklearn
 from pandas import read_csv
 from joblib import dump
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
+mlflow.set_tracking_uri("http://localhost:5555")
 workspace = os.getenv('GITHUB_WORKSPACE')
 
 # Define the directory where your Python script is located (ModelCleaning)
@@ -25,15 +28,35 @@ df = read_csv(csv_file_path)
 # Proceed with your training logic
 print(df.head()) 
 
-
-
-
 X= df["Age"].values.reshape(10,1)
 y= df["Salary"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-mind = LinearRegression()
-mind.fit(X_train,  y_train)
+
+with mlflow.start_run():
+
+  mind = LinearRegression()
+  mind.fit(X_train,  y_train)
+
+  #Evaluate
+  predictions= mind.predict(X_test)
+  r2 = r2_score(y_test, predictions)
+  mse = mean_squared_error(y_test, predictions)
+
+  #log metrics
+  mlflow.log_metric("r2_score", r2)
+  mlflow.log_metric("mse", mse)
+  # log model and register
+  result = mlflow.sklearn.log_model(sk_model=mind, artifact_path="model")
+
+  mlflow.register_model(
+    model_uri=result.model_uri, 
+    name = "my_linear_model"
+    )
+  print(f"Model logged with r2: {r2}")
+
+with open("model.pkl", "wb") as file:
+  pickle.dump(mind, file)
 
 dump(mind, "AgeSalaryModel.pkl")
